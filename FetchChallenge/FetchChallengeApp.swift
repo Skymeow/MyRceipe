9de1f935a4422e7f4b76b49591578a0bc2e7eb3f
S J <sj@Ss-MacBook-Pro.local>
2024-01-08 18:32:30 +0800

    initial setup

---
+++ FetchChallenge/FetchChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchChallengeApp.swift
+//  FetchChallenge
+//
+//  Created by S J on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
